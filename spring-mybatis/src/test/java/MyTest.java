59d52ad828801645b3341f1f8203483caee27d7b
minedegithubhao <824737880@qq.com>
2023-09-05 19:47:22 +0800

    m

---
+++ spring-mybatis/src/test/java/MyTest.java
@@ -0,0 +1,30 @@
+import org.example.mapper.UserMapper;
+import org.example.pojo.User;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MyTest {
+
+    @Test
+    public void test() throws IOException {
+
+//        String resource = "mybatis-config.xml";
+//        InputStream inputStream = Resources.getResourceAsStream(resource);
+//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+//
+//        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+//        List<User> users = mapper.selectUsers();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserMapper userMapper = context.getBean("userMapper" ,UserMapper.class);
+        List<User> users = userMapper.selectUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+
+    }
+}
