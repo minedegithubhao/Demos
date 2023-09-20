e2ad442238ceeb18042fce4b08cbde6eecdf1dc8
minedegithubhao <824737880@qq.com>
2023-09-20 21:54:37 +0800

    m

--- java-nio/src/main/java/org/example/ChatHomeDemo/ChatClient.java
+++ java-nio/src/main/java/org/example/ChatHomeDemo/ChatClient.java
@@ -16,14 +16,19 @@ public class ChatClient {
     private static final int BUFFER_SIZE = 1024;
 
     public static void main(String[] args) throws IOException {
+        // 创建channel，并配置服务端的连接信息
         SocketChannel clientChannel = SocketChannel.open(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
+        // 将channel设置为非阻塞
         clientChannel.configureBlocking(false);
 
+        // 创建一个线程，不断的从buffer中读取数据
         Thread readThread = new Thread(() -> {
             try {
                 ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
                 while (true) {
+                    // 将buffer中的数据读取到channel中
                     int bytesRead = clientChannel.read(buffer);
+                    // 如果bytesRead = -1 说明
                     if (bytesRead == -1) {
                         System.out.println("Server has disconnected.");
                         clientChannel.close();
@@ -31,14 +36,18 @@ public class ChatClient {
                     }
                     String message = new String(buffer.array(), 0, bytesRead);
                     System.out.println("Received: " + message);
+                    // 为防止刷屏，这里设置成每5秒钟从buffer中读取一次
+                    Thread.sleep(5000);
+                    // 每次读取完成后清空buffer
                     buffer.clear();
                 }
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
         });
         readThread.start();
 
+        // 从键盘录入
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String message = scanner.nextLine();
@@ -46,6 +55,7 @@ public class ChatClient {
                 clientChannel.close();
                 break;
             }
+            // 往buffer中写入数据
             clientChannel.write(ByteBuffer.wrap(message.getBytes()));
         }
     }
--- java-nio/src/main/java/org/example/ChatHomeDemo/ChatServer.java
+++ java-nio/src/main/java/org/example/ChatHomeDemo/ChatServer.java
@@ -20,7 +20,7 @@ public class ChatServer {
     private static final int BUFFER_SIZE = 1024;
 
     public static void main(String[] args) throws IOException {
-        // 创建选择器
+        // 启selector,并注册accept事件
         Selector selector = Selector.open();
         // 打开ServerSocketChannel
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
@@ -28,29 +28,30 @@ public class ChatServer {
         serverSocketChannel.bind(new InetSocketAddress(PORT));
         // 设置为非阻塞模式
         serverSocketChannel.configureBlocking(false);
-        // 向选择器注册接受连接事件
+        // 向选择器注册，并告诉选择器监听该通道的OP_ACCEPT事件
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
         System.out.println("Chat Server is running on port " + PORT);
 
         while (true) {
-            // 阻塞等待就绪事件
+            // 阻塞等待2s，监听所有通道
             int readyChannels = selector.select(2000);
             System.out.println("等待链接");
+            // readyChannels = 0，则说明未见听到就绪的事件
             if (readyChannels == 0) {
                 System.out.println("未发现链接");
                 continue;
             }
 
-            // 发现链接，获取已就绪事件的键集合
+            // 监听到就绪的事件，则获取已就绪事件的键集合
             Set<SelectionKey> selectedKeys = selector.selectedKeys();
             Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
 
-            // 遍历链接
+            // 遍历就绪时间
             while (keyIterator.hasNext()) {
                 SelectionKey key = keyIterator.next();
 
-                // 判断该SelectionKey可以接受客户端连接
+                // 判断该SelectionKey对应的channel是否可以接受客户端连接
                 if (key.isAcceptable()) {
                     // 处理连接
                     handleAcceptable(key, selector);
@@ -65,9 +66,9 @@ public class ChatServer {
     }
 
     private static void handleAcceptable(SelectionKey key, Selector selector) throws IOException {
-        // 通过key获取ServerSocketChannel
+        // 通过SelectionKey获取channel
         ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
-        // 获取客户端未非阻塞模式
+        // 根据服务端通道ServerSocketChannel获取对应的客户端通道SocketChannel
         SocketChannel clientChannel = serverChannel.accept();
         // 设置客户端通道为非阻塞模式
         clientChannel.configureBlocking(false);
@@ -78,29 +79,40 @@ public class ChatServer {
     }
 
     private static void handleReadable(SelectionKey key, Selector selector) throws IOException {
-        //
+        // 通过SelectionKey获取channel
         SocketChannel clientChannel = (SocketChannel) key.channel();
+        // 创建缓冲区，并设置缓冲区大小
         ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        // 从缓冲区中读取数据
         int bytesRead = clientChannel.read(buffer);
+        // bytesRead = -1 说明缓冲区中没有数据
         if (bytesRead == -1) {
             key.cancel();
             clientChannel.close();
             System.out.println("Client disconnected: " + clientChannel.getRemoteAddress());
             return;
         }
+        // 缓冲区有数据则读取所有数据
         String message = new String(buffer.array(), 0, bytesRead);
         System.out.println("Received from " + clientChannel.getRemoteAddress() + ": " + message);
 
-        // Broadcast the message to all connected clients
+        // 将接收到的数据广播给所有的channel
         broadcastMessage(selector, clientChannel, message);
     }
 
     private static void broadcastMessage(Selector selector, SocketChannel sender, String message) throws IOException {
+        // 获取selector中注册的所有channel
         Set<SelectionKey> keys = selector.keys();
         for (SelectionKey key : keys) {
-            if (key.isValid() && key.channel() instanceof SocketChannel && key.channel() != sender) {
+            if (key.isValid() && key.channel() instanceof SocketChannel
+                    // 广播的channel中不包括发送者
+                    && key.channel() != sender) {
+
+                //根据SelectionKey获取对应的channel
                 SocketChannel clientChannel = (SocketChannel) key.channel();
+                // 将message写入到缓冲区
                 ByteBuffer buffer = ByteBuffer.wrap(message.getBytes());
+                // 将数据写入到buffer中
                 clientChannel.write(buffer);
             }
         }
